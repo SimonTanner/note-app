101a5f4c365a6194c5adcc4eb6cbce9f0a0d7ae0
SimonTanner <simonthetanner@hotmail.com>
2017-05-22 14:28:09 +0100

    2nd test for Note.text written, test fails

--- note-model.js
+++ note-model.js
@@ -1,3 +1,5 @@
-(function(){
-  console.log("hi");
-})();
+function Note() {
+  
+}
+
+// module.exports = Note;
--- spec/note-modelSpec.js
+++ spec/note-modelSpec.js
@@ -2,11 +2,37 @@
 
 (function() {
   testOne();
+  testTwo();
 })();
 
 function testOne() {
-  var note = new Note();
-  if(note === 'undefined') {
-    return "Test failed";
+  console.log('Test 1: Note() creates a new instance of a note');
+  try {
+    var note = new Note();
+    console.log("Test passed");
+  } catch(err) {
+    console.log("Test failed");
   }
-};
+}
+
+function testTwo() {
+  console.log('Test 2: Note() takes a note as an argument');
+  var note = new Note('Hello world!');
+  try {
+    if (note.text === undefined) {
+      throw(err);
+    } else {
+      console.log('Test passed');
+    }
+  } catch(err) {
+    console.log("Test failed 'note.text' is undefined");
+  }
+}
+// note = new Note();
+//
+//
+// if (note.text === 'undefined') {
+//   throw('err');
+// } else {
+//   console.log('Test passed');
+// }
