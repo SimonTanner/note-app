2d021797a9c38b1b3cf120b8df44cbab13dd70db
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-05-22 15:01:12 +0100

    tests 3 and 4 passing and refactored

--- note-model.js
+++ note-model.js
@@ -1,5 +1,12 @@
-function Note() {
-  
+function Note(text) {
+  if(typeof text === undefined) {
+    this.text = undefined;
+  } else {
+    this.text = text;
+  }
 }
 
+Note.prototype.print = function() {
+  return this.text;
+};
 // module.exports = Note;
--- spec/note-modelSpec.js
+++ spec/note-modelSpec.js
@@ -1,9 +1,14 @@
-// const Note = require("note-model");
-
 (function() {
   testOne();
   testTwo();
+  testThree();
+  testFour();
 })();
+
+function testFail(error) {
+  console.log('Test failed');
+  console.log(error);
+}
 
 function testOne() {
   console.log('Test 1: Note() creates a new instance of a note');
@@ -11,28 +16,49 @@ function testOne() {
     var note = new Note();
     console.log("Test passed");
   } catch(err) {
-    console.log("Test failed");
+    testFail(err);
   }
 }
 
 function testTwo() {
   console.log('Test 2: Note() takes a note as an argument');
-  var note = new Note('Hello world!');
   try {
+    var note = new Note('Hello world!');
     if (note.text === undefined) {
       throw(err);
     } else {
       console.log('Test passed');
     }
   } catch(err) {
-    console.log("Test failed 'note.text' is undefined");
+    testFail(err);
   }
 }
-// note = new Note();
-//
-//
-// if (note.text === 'undefined') {
-//   throw('err');
-// } else {
-//   console.log('Test passed');
-// }
+
+function testThree() {
+  console.log('Test 3: Note().text stores the argument given');
+  try {
+    var note = new Note('thing');
+    if (note.text === undefined) {
+      throw(err);
+    } else {
+      console.log('Test passed');
+    }
+  } catch(err) {
+    testFail(err);
+  }
+}
+
+function testFour() {
+  console.log('Test 4: Note().print returns the stored text');
+  try {
+    var myText = 'thing'
+    var note = new Note(myText);
+    note.print();
+    if(note.print() !== myText) {
+      throw(err);
+    }
+    console.log('Test passed');
+  } catch(err) {
+    testFail(err);
+  }
+}
